cbba49d377c287a01de352e691cfb7ad132a1a5f
Murali Elavarthi <muralis2p@gmail.com>
2026-04-21 18:01:55 +0530

    c

--- 02-ec2/Main.tf
+++ 02-ec2/Main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web_server-1" {
+resource "aws_instance" "web_server-2" {
   ami             = "ami-0220d79f3f480ecf5"
   vpc_security_group_ids = [data.aws_ssm_parameter.sg_id.value]
   instance_type   = "t2.micro"
   tags = {
-    Name = "web_server-1"
+    Name = "web_server-2"
   }
 }
